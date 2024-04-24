82d1ac2defd3d217dc80ffae570d246f056a688d
JerryWski <jarek_wasilewski@yahoo.com>
2024-04-24 22:03:01 +0200

    adding site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()]
+  site: 'https://logo-lama.pl',
+  integrations: [icon(), react(), sitemap()]
 });
